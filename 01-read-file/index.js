3346e00528197b310729745bd1bfc66bcee2a1ca
mavlekiev <mavlekiev@gmail.com>
2025-01-20 00:39:09 +0700

    init: start task HTML-builder

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(filePath);
+
+readStream.on('data', (chunk) => {
+  console.log(chunk.toString());
+});
+
+readStream.on('error', (error) => {
+  console.error('Произошла ошибка при чтении файла:', error);
+});
