f6946f693e16c3a472992c2d2642c03407f61fde
mavlekiev <mavlekiev@gmail.com>
2025-01-20 15:27:50 +0700

    feat: add solution 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,10 +5,10 @@ const filePath = path.join(__dirname, 'text.txt');
 
 const readStream = fs.createReadStream(filePath);
 
-readStream.on('data', (chunk) => {
-  console.log(chunk.toString());
+readStream.on('data', (data) => {
+  console.log(data.toString());
 });
 
-readStream.on('error', (error) => {
-  console.error('Произошла ошибка при чтении файла:', error);
+readStream.on('error', (err) => {
+  console.error('An error occurred while reading from file:', err);
 });
