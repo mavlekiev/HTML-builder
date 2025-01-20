91350a00cb646fe3a4ee443e8270e4b1e07985a2
mavlekiev <mavlekiev@gmail.com>
2025-01-20 19:24:48 +0700

    feat: add solution 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const writeStream = fs.createWriteStream(filePath);
+
+const rl = readline.createInterface({
+  input:process.stdin,
+  output:process.stdout
+});
+
+console.log('Enter text or the word "exit" to exit');
+
+rl.on('line', (input) => {
+  if (input.toString().toLowerCase().trim() === 'exit') {
+    rl.close();
+  } else {
+    writeStream.write(input + '\n');
+  }
+});
+
+rl.on('SIGCONT', () => {
+  rl.close();
+  writeStream.close(); 
+})
+
+rl.on('close', () => {
+  console.log('Recording complete. Exit');
+  writeStream.close(); 
+});
+
+
+
+
